29cc5c2bf7caf0ff86d8e69f69e150740ce434d5
Zhang-Jiayun <zhangjiayun123@gmail.com>
2024-09-22 21:57:34 +0800

    add a visualization of hand to gripper

---
+++ pc_gripper.py
@@ -0,0 +1,148 @@
+import copy
+import open3d as o3d
+import numpy as np
+from PIL import Image
+import os
+import json
+
+# Demo floder
+rgb_folder = 'demos_new/take1/rgb'
+depth_folder = 'demos_new/take1/depth'
+# The number of frame
+index_to_view = 1
+# read the keypoints file
+with open("keypoint_all_take1.json", 'r') as json_file1:
+    data = json.load(json_file1)
+
+rgb_files = sorted([f for f in os.listdir(rgb_folder) if f.endswith('.png')])
+depth_files = sorted([f for f in os.listdir(depth_folder) if f.endswith('.npy')])
+
+vis = o3d.visualization.Visualizer()
+vis.create_window()
+
+cam_intr = o3d.camera.PinholeCameraIntrinsic(
+    width=640, height=480, fx=525., fy=525., cx=319.5, cy=239.5)
+
+gripper_mesh = o3d.io.read_triangle_mesh("robotiq_arg2f_140.obj")
+
+
+def apply_transformation_to_gripper(mesh, base_position, grasp_direction):
+    translation_matrix = np.eye(4)
+    translation_matrix[:3, 3] = base_position
+
+    default_direction = np.array([0, 0, 1])
+    axis = np.cross(default_direction, grasp_direction)
+    angle = np.arccos(np.dot(default_direction, grasp_direction) /
+                      (np.linalg.norm(default_direction) * np.linalg.norm(grasp_direction)))
+
+    axis = axis / np.linalg.norm(axis)
+    K = np.array([
+        [0, -axis[2], axis[1]],
+        [axis[2], 0, -axis[0]],
+        [-axis[1], axis[0], 0]
+    ])
+    rotation_matrix = np.eye(3) + np.sin(angle) * K + (1 - np.cos(angle)) * np.dot(K, K)
+
+    transform_matrix = np.eye(4)
+    transform_matrix[:3, :3] = rotation_matrix
+    transformation = np.dot(translation_matrix, transform_matrix)
+
+    mesh_transformed = mesh.transform(transformation)
+
+    return mesh_transformed
+
+
+def calculate_gripper_pose(P1, P2, P3):
+    P_center = (P1 + P2) / 2
+    width = np.linalg.norm(P1 - P2)
+    grasp_direction = P_center - P3
+    grasp_direction = grasp_direction / np.linalg.norm(grasp_direction)
+    return P_center, width, grasp_direction
+
+rgb_file = rgb_files[index_to_view]
+depth_file = depth_files[index_to_view]
+print(rgb_file)
+
+rgb_path = os.path.join(rgb_folder, rgb_file)
+depth_path = os.path.join(depth_folder, depth_file)
+
+try:
+    keypoints_left = data[str(rgb_file)]["left"]["3d_keypoints"]
+    translation_left = np.array(data[str(rgb_file)]["left"]["translation"])
+    keypoints_left_np = np.array(keypoints_left) + translation_left
+    keypoints_left_np = keypoints_left_np * np.array([1, -1, -1])
+
+    keypoints_right = data[str(rgb_file)]["right"]["3d_keypoints"]
+    translation_right = np.array(data[str(rgb_file)]["right"]["translation"])
+    keypoints_right_np = np.array(keypoints_right) + translation_right
+    keypoints_right_np = keypoints_right_np * np.array([1, -1, -1])
+
+    p1_left = keypoints_left_np[4]
+    p2_left = keypoints_left_np[8]
+    p3_left = (keypoints_left_np[2] + keypoints_left_np[5]) / 2
+    left_center, left_width, left_direction = calculate_gripper_pose(p1_left, p2_left, p3_left)
+
+    p1_right = keypoints_right_np[4]
+    p2_right = keypoints_right_np[8]
+    p3_right = (keypoints_right_np[2] + keypoints_right_np[5]) / 2
+    right_center, right_width, right_direction = calculate_gripper_pose(p1_right, p2_right, p3_right)
+
+    left_gripper_mesh = apply_transformation_to_gripper(copy.deepcopy(gripper_mesh), keypoints_left_np[0],
+                                                        left_direction)
+    right_gripper_mesh = apply_transformation_to_gripper(copy.deepcopy(gripper_mesh), keypoints_right_np[0],
+                                                         right_direction)
+
+    connections = [
+        (0, 1), (1, 2), (2, 3), (3, 4),
+        (0, 5), (5, 6), (6, 7), (7, 8),
+        (0, 9), (9, 10), (10, 11), (11, 12),
+        (0, 13), (13, 14), (14, 15), (15, 16),
+        (0, 17), (17, 18), (18, 19), (19, 20)
+    ]
+
+    lines_left = o3d.geometry.LineSet()
+    lines_left.points = o3d.utility.Vector3dVector(keypoints_left_np)
+    lines_left.lines = o3d.utility.Vector2iVector(connections)
+    colors_left = [[1, 0, 0] for _ in range(len(connections))]
+    lines_left.colors = o3d.utility.Vector3dVector(colors_left)
+
+    lines_right = o3d.geometry.LineSet()
+    lines_right.points = o3d.utility.Vector3dVector(keypoints_right_np)
+    lines_right.lines = o3d.utility.Vector2iVector(connections)
+    colors_right = [[0, 0, 1] for _ in range(len(connections))]
+    lines_right.colors = o3d.utility.Vector3dVector(colors_right)
+
+
+except KeyError:
+    print("KeyError occurred, skipping this file.")
+    exit()
+
+rgb_image = np.array(Image.open(rgb_path).convert('RGB'))
+depth_image = np.load(depth_path)
+
+depth_image = np.nan_to_num(depth_image, nan=0.0)
+
+rgbd_o3d = o3d.geometry.RGBDImage.create_from_color_and_depth(
+    color=o3d.geometry.Image(rgb_image),
+    depth=o3d.geometry.Image(depth_image),
+    depth_scale=1.0,
+    depth_trunc=3.0,
+    convert_rgb_to_intensity=False
+)
+
+pc_o3d = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_o3d, cam_intr)
+pc_o3d.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
+
+vis.clear_geometries()
+vis.add_geometry(pc_o3d)
+vis.add_geometry(left_gripper_mesh)
+vis.add_geometry(right_gripper_mesh)
+vis.add_geometry(lines_left)
+vis.add_geometry(lines_right)
+
+vis.poll_events()
+vis.update_renderer()
+
+vis.run()
+vis.destroy_window()
